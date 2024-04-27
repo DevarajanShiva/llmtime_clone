2e5b28c23e868c38e089e2417381e75856cfe47c
Devarajan <48157039+DevarajanShiva@users.noreply.github.com>
2024-04-27 14:42:18 +0530

    Update mistral.py

--- models/mistral.py
+++ models/mistral.py
@@ -34,7 +34,7 @@ def get_model_and_tokenizer(model_name, cache_model=False):
         return loaded[model_name]
     tokenizer = get_tokenizer()
    
-    model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1",device_map="cpu")
+    model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1", device_map="cpu", low_cpu_mem_usage=True)
     # model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2",device_map="cpu")
     model.eval()
     if cache_model:
